c9c4e6938a0f9634e82dcf390bd196cbc1eef686
Bertruss <mikehautman@gmail.com>
2016-11-17 07:21:33 +0000

    Queue now compiles without error. needs to be tested

--- LCqueue.c
+++ LCqueue.c
@@ -1,32 +1,32 @@
 #include <stdlib.h>
-#include <LCqueue.h>
+#include "LCqueue.h"
 //simplified doubly linked-list queue
 //for some reason available queue libraries wouldn't work on the teensy.
 
 //instantiates a memory location with a new queue. takes a single value to instantiate as the first node
-LCqueue *new_queue(void);
-	struct LCqueue *temp = malloc(sizeof(struct LCqueue));
+LCqueue *new_queue(void){
+	LCqueue* temp = (LCqueue *)malloc(sizeof(LCqueue));
 	temp->front = NULL;
 	temp->back = NULL;
-	temp->elements = NULL;
+	temp->elements = 0;
 	return temp;  		
 }
 
-int pop(const LCqueue* q){
+int pop(LCqueue* q){
 	int temp;
-	struct node *tempnode;
-	if(q.elements == 0){
+	struct node *tempNode;
+	if(q->elements == 0){
 		return NULL;
 	}
 	else{		
 		//decrements # of elements
-		q.elements = q.elements - 1;
+		q->elements = q->elements - 1;
 		
 		//pulls the value of the front node
-		temp = q->front.value;
+		temp = q->front->value;
 		
 		//pulls the pointer to the next node from the front
-		tempNode = q->front.trailing;
+		tempNode = q->front->trailing;
 		
 		if(tempNode != NULL){
 		//clears the leading pointer on the next node 
@@ -36,7 +36,7 @@ int pop(const LCqueue* q){
 		free(q->front);
 
 		//sets the new front node
-		q.front = tempNode;
+		q->front = tempNode;
 		
 		return temp;
 		}
@@ -44,10 +44,9 @@ int pop(const LCqueue* q){
 	}		
 }
 
-void push(const LCqueue* q, int x){
+void push(LCqueue* q, int x){
 	//allocating memory
-	struct node *temp = malloc(sizeof(struct node));
-	*temp =  struct node real; 
+        struct node *temp = malloc(sizeof(struct node)); 
 	temp->value = x;
 
 	//linking all necessary links
@@ -56,8 +55,8 @@ void push(const LCqueue* q, int x){
 	temp->leading = q->back;
 
 	//linking backwards
-	q->back.trailing = temp;
-	q.back = temp;
+	q->back->trailing = temp;
+	q->back = temp;
 
 	//incrementing "elements" count
 	q->elements = q->elements + 1; 
@@ -67,11 +66,11 @@ int count(const LCqueue* q){
 	return q->elements;
 }
 
-int end(const LCqueue* q){
-	return q->front.value;
-}
+int front(const LCqueue* q){
+	return q->front->value;
+ }
+
 
 int end(const LCqueue* q){
-	return q->back.value;
+	return q->back->value;
 }
- 
--- LCqueue.h
+++ LCqueue.h
@@ -1,26 +1,27 @@
-#ifndef _LCQUEUE_
-#define _LCQUEUE_
+#ifndef _LCQUEUE_H_
+#define _LCQUEUE_H_
 //queue structure
-typedef struct{
-node *front;
-node *back;
-int elements;
-} LCqueue;
 
 //doubly linked nodes
 typedef struct node{
-node *leading;
-node *trailing;
-int value;
+ struct node *leading;
+ struct node *trailing;
+ int value;
 };
+
+typedef struct LCqueue{
+ struct node *front;
+ struct  node *back;
+ int elements;
+} LCqueue;
 
 LCqueue *new_queue(void);
 
 //removes first element and returns value
-int pop(const LCqueue* q);
+int pop(LCqueue* q);
 
 //adds value to end of queue
-void push(const rLCqueue* q,int x);
+void push(LCqueue* q,int x);
 
 //returns number of elements in the queue
 int count(const LCqueue* q);
